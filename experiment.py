477c1d9b46ce1ac880e3a1feb6700d09f2d69123
Yuanxi Sun <43052183+sunyuanxi@users.noreply.github.com>
2021-01-23 12:45:47 -0500

    attempt to add Dummy dataset

--- experiment.py
+++ experiment.py
@@ -162,7 +162,9 @@ class VAEXperiment(pl.LightningModule):
         elif self.params['dataset'] == 'cosmo':
             dataset = CosmoData(train='train')
 
-            # to do: create dummy dataset here
+        ### Create dummy dataset for test ###
+        elif self.params['dataset'] == 'dummy':
+            dataset = DummyData()
 
         else:
             raise ValueError('Undefined dataset type')
@@ -188,14 +190,21 @@ class VAEXperiment(pl.LightningModule):
                                                 drop_last=True)
             self.num_val_imgs = len(self.sample_dataloader)
 
-        # to do: create dummy dataset here
-
         elif self.params['dataset'] == 'cosmo':
             self.sample_dataloader = DataLoader(CosmoData(train='val'),
                                                 batch_size=self.params['batch_size'],
                                                 num_workers=self.params['num_workers'],
                                                 shuffle=False)
             self.num_val_imgs = len(self.sample_dataloader)
+        
+        ### Create dummy dataset for test ###
+        elif self.params['dataset'] == 'dummy':
+            self.sample_dataloader = DataLoader(DummyData(),
+                                                batch_size=self.params['batch_size'],
+                                                num_workers=self.params['num_workers'],
+                                                shuffle=False)
+            self.num_val_imgs = len(self.sample_dataloader)
+        
         else:
             raise ValueError('Undefined dataset type')
 
