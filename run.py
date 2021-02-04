3f840123f7e58141d8c2da23466ebc667ecfc943
Yuanxi Sun <43052183+sunyuanxi@users.noreply.github.com>
2021-02-04 05:48:48 -0500

    Add nn_reg part in run.py but failed

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from models import *
 from experiment import VAEXperiment
+from nn_reg_experiment import RegExperiment
 import torch.backends.cudnn as cudnn
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TestTubeLogger
@@ -47,3 +48,17 @@ runner = Trainer(default_root_dir=f"{tt_logger.save_dir}",
 
 print(f"======= Training {config['model_params']['name']} =======")
 runner.fit(experiment)
+
+# nn_reg part
+# nn_reg_model = vae_models[config['reg_params']['name']](**config['reg_params'])
+# nn_reg_experiment = RegExperiment(experiment.model.encode, nn_reg_model,
+#                                   config['reg_exp_params'])
+
+# nn_reg_runner = Trainer(default_root_dir=f"{tt_logger.save_dir}",
+#                         # logger=tt_logger,
+#                         val_check_interval=1.,
+#                         num_sanity_val_steps=5,
+#                         **config['trainer_params'])
+
+# print(f"======= Training {config['reg_params']['name']} =======")
+# nn_reg_runner.fit(nn_reg_experiment)
