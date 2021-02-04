0af7e2eac15a00323510d52bd4b41a51f52fd87e
Yuanxi Sun <43052183+sunyuanxi@users.noreply.github.com>
2021-02-04 05:52:35 -0500

    Update neural_net_reg.py

--- models/neural_net_reg.py
+++ models/neural_net_reg.py
@@ -4,39 +4,9 @@ from torch import nn
 from torch.nn import functional as F
 from .types_ import *
 
-from sklearn.metrics import r2_score
+# from sklearn.metrics import r2_score
 
-class model_3hl(nn.Module):
-    
-    def __init__(self, inp, h1, h2, h3, out, dr):
-        super(model_3hl, self).__init__()
-        
-        self.fc1 = nn.Linear(inp, h1) 
-        self.fc2 = nn.Linear(h1,  h2)
-        self.fc3 = nn.Linear(h2,  h3)
-        self.fc4 = nn.Linear(h3,  out)
-        self.dropout   = nn.Dropout(p=dr)
-        self.ReLU      = nn.ReLU()
-        self.LeakyReLU = nn.LeakyReLU()
-        
-        # initialize the weights of the different layers
-        for m in self.modules():
-            if isinstance(m, nn.BatchNorm3d) or isinstance(m, nn.BatchNorm1d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 1)
-            elif isinstance(m, nn.Conv3d) or isinstance(m, nn.ConvTranspose2d) or \
-                 isinstance(m, nn.Linear):
-                nn.init.kaiming_normal_(m.weight)
-       
-    # forward pass
-    def forward(self, x):
-        out = self.dropout(self.LeakyReLU(self.fc1(x)))
-        out = self.dropout(self.LeakyReLU(self.fc2(out)))
-        out = self.dropout(self.LeakyReLU(self.fc3(out)))
-        out = self.fc4(out)         
-        return out
-
-class VanillaVAE(BaseVAE):
+class NeuralNetReg(BaseVAE):
 
     def __init__(self,
                  input_dim: int,
@@ -44,18 +14,15 @@ class VanillaVAE(BaseVAE):
                  drop_out: float,
                  hidden_dims: List = None,
                  **kwargs) -> None:
-        super(VanillaVAE, self).__init__()
-
-        self.latent_dim = latent_dim
+        super(NeuralNetReg, self).__init__()
 
         modules = []
         if hidden_dims is None:
             hidden_dims = [100, 100, 100]
 
-        # Build Encoder
         for h_dim in hidden_dims:
             modules.append(
-                nn.Squential(
+                nn.Sequential(
                     nn.Linear(input_dim, h_dim),
                     nn.LeakyReLU(),
                     nn.Dropout(p=drop_out))
@@ -68,6 +35,14 @@ class VanillaVAE(BaseVAE):
     def forward(self, input: Tensor, **kwargs) -> Tensor:
         output = self.model(input)
         return self.final_layer(output)
+
+    def r2_score(self, y_pred, y):
+        n = y.shape[0]
+        sum_of_err = torch.sum(torch.pow(y_pred - y, 2)).item()
+        y_sum = torch.sum(y).item()
+        y_sq_sum = torch.sum(torch.pow(y, 2)).item()
+        
+        return 1 - sum_of_err / (y_sq_sum - (y_sum ** 2) / n)
 
     def loss_function(self,
                       *args,
@@ -83,5 +58,5 @@ class VanillaVAE(BaseVAE):
         input = args[1]
 
         rmse_loss = torch.sqrt(F.mse_loss(preds, input))
-        r2_score = r2_score(preds, input)
+        r2_score = self.r2_score(preds, input)
         return {'RMSE_loss': rmse_loss, 'R_squared_score': r2_score}
